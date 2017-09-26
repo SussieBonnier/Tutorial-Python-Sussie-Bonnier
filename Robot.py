deed3c931af7966e690bd61bb2b9864698de4978
SussieBonnier <32262172+SussieBonnier@users.noreply.github.com>
2017-09-26 15:04:58 +0200

    Funktion inne i klass

--- Robot.py
+++ Robot.py
@@ -18,6 +18,8 @@ class Product:
 	price=0
 	count=0
 	tax=1.25
+	def price_with_tax(self):
+		return self.price * self.count * self.tax
 robot=Product()
 robot.price=900
 robot.count=1
@@ -26,4 +28,6 @@ book=Product()
 book.price=100
 book.count=1
 book.tax=1.06
-print(robot.price*robot.count*robot.tax+book.price*book.count*book.tax)
+# print(robot.price*robot.count*robot.tax+book.price*book.count*book.tax)
+ 
+print(robot.price_with_tax()+book.price_with_tax())
