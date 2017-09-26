d25646de9db8af325dc8cfcd8a9622248b846889
SussieBonnier <32262172+SussieBonnier@users.noreply.github.com>
2017-09-26 14:44:27 +0200

    Created a product class

---
+++ Robot.py
@@ -0,0 +1,29 @@
+# print(900 * 200 * 1.25 + 100 * 1.06)
+# robot_price = 900
+# robot_count = 2
+# robot_tax = 1.25
+# book_price = 100
+# book_count = 2
+# book_tax = 1.06
+# print(robot_price*robot_count*robot_tax+book_price*book_count*book_tax)
+
+# robot={
+# "price":900,
+# "count":2,
+# "tax":1.25,
+# }
+# print(robot["price"] *robot["count"]*robot["tax"])
+
+class Product:
+	price=0
+	count=0
+	tax=1.25
+robot=Product()
+robot.price=900
+robot.count=1
+robot.tax=1.25
+book=Product()
+book.price=100
+book.count=1
+book.tax=1.06
+print(robot.price*robot.count*robot.tax+book.price*book.count*book.tax)
